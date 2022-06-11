fbda82162b8f76d737cd669e5a6262712fcd189c
nuyhc <spec327@naver.com>
2022-06-12 04:36:38 +0900

    test

--- 8. CAoMDDaHE/for_distribute/fd_main.py
+++ 8. CAoMDDaHE/for_distribute/fd_main.py
@@ -8,4 +8,14 @@ import chart_studio
 import folium
 from folium.plugins import MarkerCluster
 import json
-import Stramlit
+import Stramlit as st
+
+## Title
+st.title("주요 질병 사망자 및 보건 환경 비교 분석")
+
+## Header
+st.header("멋쟁이 사자처럼 AI School 6기 MID Project 2팀")
+st.text("팀원: 김재석, 김채현, 김태훈, 박이슬, 손유선")
+
+## Markdown
+st.markdown("# ")
